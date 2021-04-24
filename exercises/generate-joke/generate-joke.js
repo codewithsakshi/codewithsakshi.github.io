cdec43b89a77324faec15e0ac24f5c04a85a285a
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-04-24 21:30:16 +0530

    add generate joke nd geographic page

---
+++ exercises/generate-joke/generate-joke.js
@@ -0,0 +1,8 @@
+const jokeEl = document.querySelector(".joke");
+const jokeBtn = document.querySelector(".joke-btn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+// USING ASYNC/AWAIT

4de28bedc2f4ecff9bb2f79ba6a3fb2c054a533f
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-04-26 14:33:17 +0530

    fixed javascript

--- exercises/generate-joke/generate-joke.js
+++ exercises/generate-joke/generate-joke.js
@@ -6,3 +6,16 @@ jokeBtn.addEventListener("click", generateJoke);
 generateJoke();
 
 // USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+}

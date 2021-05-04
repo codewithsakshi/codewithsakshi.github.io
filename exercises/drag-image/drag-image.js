52cd88ccbef503bec0d75a817877a24f50be53e3
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-05-05 00:24:17 +0530

    add todolist, theme clock, notes app, github profile, blurry loading

--- exercises/drag-image/drag-image.js
+++ exercises/drag-image/drag-image.js
@@ -1,39 +1,41 @@
-// const fill = document.querySelector(".fill");
-// const empties = document.querySelectorAll(".empty");
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
 
-// fill.addEventListener("dragstart", dragStart);
-// fill.addEventListener("dragend", dragEnd);
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
 
-// for (const empty of empties) {
-//   empty.addEventListener("dragover", dragOver);
-//   empty.addEventListener("dragenter", dragEnter);
-//   empty.addEventListener("dragleave", dragLeave);
-//   empty.addEventListener("drop", dragDrop);
-// }
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
 
-// function dragStart() {
-//   this.className += " hold";
-//   setTimeout(() => (this.className = "invisible"), 0);
-// }
+function dragStart() {
+  this.className = this.className + " hold";
+  setTimeout(function () {
+    (this.className = "invisible"), 0;
+  });
+}
 
-// function dragEnd() {
-//   this.className = "fill";
-// }
+function dragEnd() {
+  this.className = "fill";
+}
 
-// function dragOver(e) {
-//   e.preventDefault();
-// }
+function dragOver(e) {
+  e.preventDefault();
+}
 
-// function dragEnter(e) {
-//   e.preventDefault();
-//   this.className += " hovered";
-// }
+function dragEnter(e) {
+  e.preventDefault();
+  this.className = this.className + " hovered";
+}
 
-// function dragLeave() {
-//   this.className = "empty";
-// }
+function dragLeave() {
+  this.className = "empty";
+}
 
-// function dragDrop() {
-//   this.className = "empty";
-//   this.append(fill);
-// }
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}

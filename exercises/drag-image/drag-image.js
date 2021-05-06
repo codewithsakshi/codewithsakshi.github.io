05e254139db025daca5d0747649ed8d58d9d5b3e
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-05-06 23:24:12 +0530

    add sticky navigation my website and hollywood movie app in personal portfolio

--- exercises/drag-image/drag-image.js
+++ exercises/drag-image/drag-image.js
@@ -1,41 +1,52 @@
-const fill = document.querySelector(".fill");
-const empties = document.querySelectorAll(".empty");
+const fillContainer = document.querySelector(".fill");
+const emptyContainers = document.querySelectorAll(".empty");
 
-fill.addEventListener("dragstart", dragStart);
-fill.addEventListener("dragend", dragEnd);
+fillContainer.addEventListener("dragstart", dragStart);
+fillContainer.addEventListener("dragend", dragEnd);
 
-for (const empty of empties) {
-  empty.addEventListener("dragover", dragOver);
-  empty.addEventListener("dragenter", dragEnter);
-  empty.addEventListener("dragleave", dragLeave);
-  empty.addEventListener("drop", dragDrop);
+for (let i = 0; i < emptyContainers.length; i++) {
+  emptyContainers[i].addEventListener("dragover", dragOver);
+  emptyContainers[i].addEventListener("dragenter", dragEnter);
+  emptyContainers[i].addEventListener("dragleave", dragLeave);
+  emptyContainers[i].addEventListener("drop", dragDrop);
 }
 
 function dragStart() {
-  this.className = this.className + " hold";
-  setTimeout(function () {
-    (this.className = "invisible"), 0;
-  });
+  console.log("DRAG started");
+  console.log(this);
+  const fillContainerContext = this;
+  this.classList.add("hold");
+  setTimeout(
+    function () {
+      this.className = "invisible";
+    }.bind(fillContainerContext),
+    0
+  );
 }
 
 function dragEnd() {
+  console.log("Drag ended");
   this.className = "fill";
 }
 
 function dragOver(e) {
+  console.log("drag over");
   e.preventDefault();
 }
 
 function dragEnter(e) {
+  console.log("drag enter");
   e.preventDefault();
   this.className = this.className + " hovered";
 }
 
 function dragLeave() {
+  console.log("drag leave");
   this.className = "empty";
 }
 
 function dragDrop() {
+  console.log("drag drop");
   this.className = "empty";
-  this.append(fill);
+  this.append(fillContainer);
 }

535b7a0f81079e31e1d71e1cdef20168cdf8da27
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-05-09 22:05:11 +0530

    fixed tribute page for mobile application

--- exercises/animated-countdown/animated-countdown.js
+++ exercises/animated-countdown/animated-countdown.js
@@ -1,24 +1,24 @@
-// const nums = document.querySelectorAll(".nums span");
-// const counter = document.querySelector(".counter");
-// const finalMessage = document.querySelector(".final");
-// const replay = document.querySelector(".replay");
+// const numsELements = document.querySelectorAll(".nums span");
+// const counterElement = document.querySelector(".counter");
+// const finalMessageElement = document.querySelector(".final");
+// const replayBtnElement = document.querySelector(".replay");
 
 // runAnimation();
 
 // function resetDOM() {
-//   counter.classList.remove("hide");
-//   finalMessage.classList.remove("show");
+//   counterElement.classList.remove("hide");
+//   finalMessageElement.classList.remove("show");
 
-//   nums.forEach((num) => {
+//   numsElements.forEach(function (num) {
 //     num.classList.value = "";
 //   });
 
-//   nums[0].classList.add("in");
+//   numsElements[0].classList.add("in");
 // }
 
 // function runAnimation() {
-//   nums.forEach((num, idx) => {
-//     const nextToLast = nums.length - 1;
+//   numsElements.forEach(function (num, idx) {
+//     const nextToLast = numsElements.length - 1;
 
 //     num.addEventListener("animationend", (e) => {
 //       if (e.animationName === "goIn" && idx !== nextToLast) {
@@ -27,8 +27,8 @@
 //       } else if (e.animationName === "goOut" && num.nextElementSibling) {
 //         num.nextElementSibling.classList.add("in");
 //       } else {
-//         counter.classList.add("hide");
-//         finalMessage.classList.add("show");
+//         counterElement.classList.add("hide");
+//         finalMessageElement.classList.add("show");
 //       }
 //     });
 //   });
@@ -38,3 +38,44 @@
 //   resetDOM();
 //   runAnimation();
 // });
+
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const replay = document.querySelector(".replay");
+
+runAnimation();
+
+function resetDOM() {
+  counter.classList.remove("hide");
+  finalMessage.classList.remove("show");
+
+  nums.forEach((num) => {
+    num.classList.value = "";
+  });
+
+  nums[0].classList.add("in");
+}
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    const nextToLast = nums.length - 1;
+
+    num.addEventListener("animationend", (e) => {
+      if (e.animationName === "goIn" && idx !== nextToLast) {
+        num.classList.remove("in");
+        num.classList.add("out");
+      } else if (e.animationName === "goOut" && num.nextElementSibling) {
+        num.nextElementSibling.classList.add("in");
+      } else {
+        counter.classList.add("hide");
+        finalMessage.classList.add("show");
+      }
+    });
+  });
+}
+
+replay.addEventListener("click", () => {
+  resetDOM();
+  runAnimation();
+});

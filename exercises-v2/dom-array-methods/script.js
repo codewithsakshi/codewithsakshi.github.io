c81f6e039af439415c2f558531e470d52bfd8cb5
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-05-14 08:13:17 +0530

    change user name and money

--- exercises-v2/dom-array-methods/script.js
+++ exercises-v2/dom-array-methods/script.js
@@ -1,18 +1,44 @@
 console.log("Hello World");
 
 let users = [
-  { name: "Satyam", wealth: 10000, id: 1 },
-  { name: "Satyam", wealth: 20000, id: 2 },
-  { name: "Satyam", wealth: 30000, id: 3 },
-  { name: "Satyam", wealth: 40000, id: 4 },
+  { name: "Shivam", wealth: 1000000, id: 1 },
+  { name: "Anil", wealth: 430000, id: 2 },
+  { name: "Aakash", wealth: 350000, id: 3 },
+  { name: "Sakshi", wealth: 4650000, id: 4 },
 ];
 
 const newUsers = [
-  { name: "A", wealth: 3000, id: 4 },
-  { name: "B", wealth: 4000, id: 5 },
-  { name: "C", wealth: 5000, id: 6 },
-  { name: "D", wealth: 6000, id: 8 },
-  { name: "E", wealth: 7000, id: 9 },
+  { name: "Shivani", wealth: 53000, id: 4 },
+  { name: "Sonam", wealth: 44000, id: 5 },
+  { name: "Shalini", wealth: 895000, id: 6 },
+  { name: "Ravi", wealth: 6000489, id: 8 },
+  { name: "Neelam", wealth: 700049, id: 9 },
+  { name: "Shashank", wealth: 477000, id: 10 },
+  { name: "Virat", wealth: 47000, id: 11 },
+  { name: "Rohit", wealth: 57000, id: 12 },
+  { name: "Rahul", wealth: 67000, id: 13 },
+  { name: "Vikash", wealth: 77000, id: 14 },
+  { name: "Santosh", wealth: 87000, id: 15 },
+  { name: "Hariom", wealth: 97000, id: 16 },
+  { name: "Minakshi", wealth: 75700, id: 17 },
+  { name: "Sandhya", wealth: 7600, id: 18 },
+  { name: "Raveena", wealth: 798500, id: 19 },
+  { name: "Karan", wealth: 47000, id: 20 },
+  { name: "Ananaya", wealth: 987000, id: 21 },
+  { name: "Aayush", wealth: 647000, id: 22 },
+  { name: "Sanjay", wealth: 3327000, id: 23 },
+  { name: "priti", wealth: 4337000, id: 24 },
+  { name: "Honey", wealth: 537000, id: 25 },
+  { name: "Suresh", wealth: 3227000, id: 26 },
+  { name: "Anil", wealth: 657000, id: 27 },
+  { name: "Shreya", wealth: 237000, id: 28 },
+  { name: "Arjeet", wealth: 2327000, id: 29 },
+  { name: "Arman", wealth: 3437000, id: 30 },
+  { name: "Aman", wealth: 3427000, id: 31 },
+  { name: "Shruti", wealth: 7547000, id: 32 },
+  { name: "Samiksha", wealth: 547000, id: 33 },
+  { name: "Lovely", wealth: 37000, id: 34 },
+  { name: "Komal", wealth: 437000, id: 35 },
 ];
 
 const addUserBtn = document.querySelector(".add-user-btn");

fc12cdefca93a879e965bbe52e0e35546765d314
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-05-16 00:44:34 +0530

    there is no update in random image generator and faq, only i go through it

--- exercises/random-image-generator/random-image-generator.js
+++ exercises/random-image-generator/random-image-generator.js
@@ -1,22 +1,18 @@
- const container = document.querySelector('.container')
- const unsplashURL = 'https://source.unsplash.com/random/'
- const rows = 5;
- const totalRows = rows*3;
+const container = document.querySelector(".container");
+const unsplashURL = "https://source.unsplash.com/random/";
+const rows = 5;
+const totalRows = rows * 3;
 
- for(let i = 0; i < totalRows; i++) {
- const img = document.createElement('img')
- img.src = `${unsplashURL}${getRandomSize()}`
- container.appendChild(img)
- }
-
-function getRandomSize() {
-//  return `${getRandomNr()}x${getRandomNr()}}`
- return `${getRandomNr()}x${getRandomNr()}`
+for (let i = 0; i < totalRows; i++) {
+  const img = document.createElement("img");
+  img.src = `${unsplashURL}${getRandomSize()}`;
+  container.appendChild(img);
 }
 
- function getRandomNr() {
- return Math.floor(Math.random() * 10) + 300;
- 
- }
- 
- 
+function getRandomSize() {
+  return `${getRandomNo()}x${getRandomNo()}`;
+}
+
+function getRandomNo() {
+  return Math.floor(Math.random() * 10) + 300;
+}

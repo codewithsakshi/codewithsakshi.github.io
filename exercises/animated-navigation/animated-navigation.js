f29ae23aa9bb20f6dfde4f3942583bcd9f1b4f60
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-06-13 00:23:15 +0530

    Delete animated-navigation.js

--- exercises/animated-navigation/animated-navigation.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector(".toggle");
-const nav = document.querySelector(".nav");
-
-toggle.addEventListener("click", function () {
-  nav.classList.toggle("active");
-});

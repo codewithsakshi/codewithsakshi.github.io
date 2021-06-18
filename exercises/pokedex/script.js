4895fe95eca3f6a0d5e696aa97bbe084933dc493
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-06-18 13:44:34 +0530

    add pokemon card

--- exercises/pokedex/script.js
+++ exercises/pokedex/script.js
@@ -1,64 +1,74 @@
-// const poke_container = document.getElementById("poke-container");
-// const pokemon_count = 150;
-// const colors = {
-//   fire: "#FDDFDF",
-//   grass: "#DEFDE0",
-//   electric: "#FCF7DE",
-//   water: "#DEF3FD",
-//   ground: "#f4e7da",
-//   rock: "#d5d5d4",
-//   fairy: "#fceaff",
-//   poison: "#98d7a5",
-//   bug: "#f8d5a3",
-//   dragon: "#97b3e6",
-//   psychic: "#eaeda1",
-//   flying: "#F5F5F5",
-//   fighting: "#E6E0D4",
-//   normal: "#F5F5F5",
-// };
+const poke_container = document.getElementById('poke-container');
 
-// const main_types = Object.keys(colors);
+const pokemonCount = 150;
 
-// const fetchPokemons = async () => {
-//   for (let i = 1; i <= pokemon_count; i++) {
-//     await getPokemon(i);
-//   }
-// };
+const pokemonURL = 'https://pokeapi.co/api/v2/pokemon/';
+// console.log(pokemonURL);
 
-// const getPokemon = async (id) => {
-//   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   createPokemonCard(data);
-// };
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+};
 
-// const createPokemonCard = (pokemon) => {
-//   const pokemonEl = document.createElement("div");
-//   pokemonEl.classList.add("pokemon");
+const mainTypes = Object.keys(colors);
+console.log({ mainTypes });
 
-//   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-//   const id = pokemon.id.toString().padStart(3, "0");
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getPokemon(i);
+  }
+};
 
-//   const poke_types = pokemon.types.map((type) => type.type.name);
-//   const type = main_types.find((type) => poke_types.indexOf(type) > -1);
-//   const color = colors[type];
+async function getPokemon(id) {
+  try {
+    const res = await fetch(`${pokemonURL}${id}`);
+    const data = await res.json();
+    console.log({ data });
 
-//   pokemonEl.style.backgroundColor = color;
+    createPokemonCard(data);
+  } catch (err) {
+    console.log(err.toString());
+  }
+}
 
-//   const pokemonInnerHTML = `
-//     <div class="img-container">
-//         <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
-//     </div>
-//     <div class="info">
-//         <span class="number">#${id}</span>
-//         <h3 class="name">${name}</h3>
-//         <small class="type">Type: <span>${type}</span> </small>
-//     </div>
-//     `;
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
 
-//   pokemonEl.innerHTML = pokemonInnerHTML;
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, '0');
 
-//   poke_container.appendChild(pokemonEl);
-// };
+  const pokeTypes = pokemon.types.map((type) => type.type.name);
 
-// fetchPokemons();
+  const type = mainTypes.find((type) => pokeTypes.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+      <div class="img-container">
+          <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+      </div>
+      <div class="info">
+          <span class="number">#${id}</span>
+          <h3 class="name">${name}</h3>
+          <small class="type">Type: <span>${type}</span> </small>
+      </div>
+      `;
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  poke_container.appendChild(pokemonEl);
+};
+
+fetchPokemons();

ad5819c5e5234e5918f35d0f1559f1e6c7588d73
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-06-18 17:16:15 +0530

    calculate loading value

--- exercises/blurry-loading/script.js
+++ exercises/blurry-loading/script.js
@@ -1,5 +1,5 @@
-const loadText = document.querySelector(".loading-text");
-const bg = document.querySelector(".bg");
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
 let load = 0;
 

09b1f7d17a49223e9435ec3cfdcebb4fa8678bae
Sakshi Mittal <sakshimittal3005@gmail.com>
2021-07-07 23:24:51 +0530

    add banking app

--- exercises-v2/banking-app/script.js
+++ exercises-v2/banking-app/script.js
@@ -1,228 +1,106 @@
-const resultElement = document.getElementById("result")
-const errorMessageElement = document.getElementById('errorMessage')
-const availableAccounts = document.querySelector('.available-accounts')
+const withdrawnAmountEl = document.querySelector('.amount-withdraw');
+// const errorMessageElement = document.getElementById('errorMessage');
+const resultElement = document.querySelector('.result');
 
-function CreateAccount(firstName, lastName, email){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.email = email
-    this.balance = 0
+const formEl = document.querySelector('#form');
 
-    this.deposit = function(amount){
-        if(amount < 0) {
-            errorMessageElement.innerHTML =`Deposit amount can not be less than 0`
-        } else {
-            this.balance = this.balance + Number(amount)
-        }
+const formContainerEl = document.querySelector('.form-container');
+
+const depositBtnEl = document.querySelector('.deposit-btn');
+const withdrawnBtnEl = document.querySelector('.withdrawn-btn');
+
+const modalFormEl = document.querySelector('.modal-form');
+const submitBtnEl = document.querySelector('.submit-btn');
+
+const messageEl = document.querySelector('.final-message');
+
+class CreateAccount {
+  constructor(firstName, lastName, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.balance = 0;
+  }
+
+  deposit(amount) {
+    if (amount < 0) {
+      errorMessageElement.innerHTML = `Deposit amount can not be less than 0`;
+    } else {
+      this.balance = this.balance + Number(amount);
     }
+  }
 
-    this.withdraw = function(amount){
-        if(this.balance < amount){
-            errorMessageElement.innerHTML= `Insufficient balance`
-        } else {
-            this.balance = this.balance - amount
-        }
+  withdraw(amount) {
+    if (this.balance < amount) {
+      errorMessageElement.innerHTML = `Insufficient balance`;
+    } else {
+      this.balance = this.balance - amount;
     }
+  }
 }
 
-
-if(!localStorage.getItem("accounts")) {
- localStorage.setItem("accounts", [])       
+if (!localStorage.getItem('accounts')) {
+  localStorage.setItem('accounts', []);
 }
 
-let accounts = []
-
+let accounts = [];
 
 try {
-    accounts = JSON.parse(localStorage.getItem("accounts"))
-} catch(err){
-    accounts = []
-    localStorage.setItem("accounts", [])
+  accounts = JSON.parse(localStorage.getItem('accounts'));
+} catch (err) {
+  accounts = [];
+  localStorage.setItem('accounts', []);
 }
-let account = null
+let account = null;
 
-function updateLocalStorage(){
-    localStorage.setItem("accounts", JSON.stringify(accounts))
-}
-
-
-function foo(){
-        let firstName = prompt("Enter first name")
-        let lastName = prompt("Enter Last Name")
-        let email = prompt("Enter email")
-        
-        account = new CreateAccount(firstName, lastName, email)
-        accounts.push(account)
-        updateLocalStorage()
-        loadAllAccounts()
-        resultElement.innerHTML = `Account created successfully`
+function updateLocalStorage() {
+  localStorage.setItem('accounts', JSON.stringify(accounts));
 }
 
-let input = null
+// formEl.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const formData = new FormData(formEl);
+//   const data = Object.fromEntries(formData);
 
-function loadAllAccounts(){
-    console.log(accounts)
-    accounts.forEach(account => {
-        const accountElement = document.createElement('div')
-        accountElement.innerHTML = `
-        <p><strong>Name<strong>: ${account.firstName} ${account.lastName}</p>
-        <p><strong>Balance<strong>: ${account.balance}</p>
-        `
-        availableAccounts.appendChild("accountElement")
-    })
-}
+//   const { firstName, lastName, email } = data;
+//   console.log({ firstName, lastName, email });
 
-function askChoice(){
-    input = prompt("Enter your choice")
-    switch(input){
-        case '1': foo()
-        break;
-        case '2':
-            if(!account) {
-                errorMessageElement.innerHTML = `You don't have account. press 1 to create account`
-            } else {
-                let amount = prompt("Enter amount to deposit")
-                account.deposit(amount)
-                resultElement.innerHTML = `Amount ${amount} deposited successfully`
-            }
-            break;
-        case '3':
-            if(!account) {
-                errorMessageElement.innerHTML = `You don't have account. press 1 to create account`
-            } else {
-                let amount = prompt("Enter amount to withdraw")
-                account.withdraw(amount)
-                resultElement.innerHTML = `Amount ${amount} withdrawn successfully`
-            }
-            break;
-        case '4':
-            if(!account) {
-                errorMessageElement.innerHTML = `You don't have account. press 1 to create account`
-            } else {
-                resultElement.innerHTML = `You have ${account.balance} left in your account`
-            }
-            break;
-        case '9':
-            resultElement.innerHTML = `Thank you for using our banking app. Hope to see you again!`
-            break;
-        default:
-            errorMessageElement.innerHTML = `Invalid choice`
-    }
-}
+//   account = new CreateAccount(firstName, lastName, email);
+//   accounts.push(account);
+//   updateLocalStorage();
+//   resultElement.innerHTML = `Account created successfully. Redirecting to home page`;
+//   setTimeout(() => {
+//     window.location.href = '/exercises-v2/banking-app/home.html';
+//   }, 2000);
+// });
 
-askChoice()
-loadAllAccounts()
+depositBtnEl.addEventListener('click', () => {
+  modalFormEl.classList.add('show');
+  submitBtnEl.innerHTML = 'Add';
+});
 
-// function CreateAccount(firstName, lastName, email) {
-//     this.firstName = firstName
-//     this.lastName = lastName
-//     this.email = email
-//     this.balance = 0
+withdrawnBtnEl.addEventListener('click', (e) => {
+  modalFormEl.classList.add('show');
+  submitBtnEl.innerHTML = 'Withdraw';
+});
 
-//     this.deposit = function(amount) {
-//         if(amount < 0) {
-//             errorMessageElement.innerHTML = `Deposit can not be less than zero`
-//         }else {
-//             this.balance = this.balance + Number()
-//         }
-//     }
+// const cashWithdrawnBtnEl = document.querySelector('.cash-withdrawn');
+// const addAmountBtnEl = document.querySelector('.add-amount');
 
-//     this.withdraw = function(amount) {
-//         if(this.balance < amount ) {
-//             errorMessageElement.innerHTML = `Insufficient balance`
-//         }else {
-//             this.balance = this.balance - amount
-//         }
-//     }
-    
-// }
+// cashWithdrawnBtnEl.addEventListener('click', () => {
+//   console.log('i clicked');
+//   console.log('amount withdrawn');
+//   messageEl.innerHTML = `collect your cash, your amount withdrawn successfully`;
+// });
 
-// if(!localStorage.getItem("accounts")) {
-//     localStorage.setItem("accounts", [])
-// }
+// addAmountBtnEl.addEventListener('click', () => {
+//   console.log('i clicked');
+//   console.log('amount withdrawn');
+//   messageEl.innerHTML = `your amount deposit successfully`;
+// });
 
-// let accounts = []
-
-// try {
-//     accounts = JSON.parse(localStorage.getItem('accounts'))
-//  }catch(error) {
-//      accounts = []
-//      localStorage.setItem("accounts", [])
-//  }
-//  let account = null
- 
-//  function updateLocalStorage() {
-//      localStorage.setItem("accounts", JSON.stringify(accounts))
-//  }
- 
-//  function foo() {
-//     let firstName = prompt("Enter first name")
-//     let lastName = prompt("Enter last name")
-//     let email = prompt("Enter your email")
-
-//     account = new CreateAccount(firstName, lastName, email)
-//     accounts.push(account)
-//     updateLocalStorage()
-//     loadAllAccounts()
-//     resultElement.innerHTML = `Account created successfully`
-// }
-
-// let input = null 
-// function loadAllAccounts() {
-//     accounts.forEach(accounts => {
-//         const accountElement = document.createElement("div")
-//         accountElement.innerHTML = `
-//         <p><strong>Name:</strong>${account.firstName} ${account.lastName}</p>
-//         <p><strong>Balance:${account.balance}</p>`
-
-//         availableAccounts.appendChild("accountElement")
-//     })
-// }
-
-
-// function askChoice() {
-// let input = prompt("Enter your choice here")
-// switch(input) {
-//     case "1": foo()
-//     break;
-
-//     case '2': 
-//     if(!account) {
-//         errorMessageElement.innerHTML = `you don't have account. press 1 to create account`
-//     }else {
-//        let account = prompt("Enter amount to deposit") 
-//        account.deposit(amount)
-//        resultElement.innerHTML = `Amount  ${amount} deposit successfully`
-//     }
-//     break;
-
-//     case '3': 
-//     if(!account) {
-//         errorMessageElement.innerHTML = `you don't have account. press 1 to create account`
-//     }else {
-//         let account = prompt("Enter amount to withdrawn")
-//         account.withdraw(amount)
-//         resultElement.innerHTML = `Ammount ${amount} withdrawn suuccefully`
-//     }
-//     break;
-
-//     case '4': 
-//     if(!account) {
-//         errorMessageElement.innerHTML = `you don't have account. press 1 to create account`
-//     } else {
-//       resultElement.innerHTML = `You have ${account.balance} left in your account`
-//     }
-//     break;
-
-//     case '9':
-//         if(!account) {
-//             errorMessageElement.innerHTML = `Thank you for using our banking app. Hope to see you again!`
-//         }
-//         break;
-//         default:
-//         errorMessageElement.innerHTML = `Invalid choice`
-//      }
-// }
-
-// askChoice()
-// loadAllAccounts()
+modalFormEl.addEventListener('submit', (e) => {
+  modalFormEl.classList.remove('show');
+  e.preventDefault();
+  messageEl.innerHTML = `your transaction succesed`;
+});
